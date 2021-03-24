b4133667a2a65698fd96f98828cdcf7dfe1ee747
Dial <tharindugamage0414@gmail.com>
2021-03-23 19:41:03 -1000

    single pos page 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -46,7 +46,16 @@ btnOrder.addEventListener('click', function () {
     customer.style.display = 'none';
     item.style.display = 'none';
     order.style.display = 'block';
+    setCurrentDate();
+    setOrderId();
+    addItemToDropDown();
+    addCustomerToDropDown();
 })
+
+
+var items = new Array();
+var customers = new Array();
+var orders = new Array();
 
 /*-----------------------Customer---------------------------------*/
 
@@ -63,9 +72,19 @@ function saveCustomer() {
     let cusMobile = $("#txtPhoneNo").val();
     let cusEmail = $("#txtEmail").val();
 
-
     let row = `<tr><td>${cusId}</td><td>${cusName}</td><td>${cusAddress}</td><td>${cusMobile}</td><td>${cusEmail}</td></tr>`;
+
     $('#tblCustomer').append(row);
+
+    let customer = {
+        cusId:cusId,
+        cusName:cusName,
+        cusAddress:cusAddress,
+        cusMobile:cusMobile,
+        cusEmail:cusEmail,
+    }
+
+    customers.push(customer);
 
     clearCustomerTextFields();
 
@@ -118,13 +137,11 @@ $("#txtEmail").on('keydown',function (event) {
     }
 });
 
-$("#txtCustomerId,#txtCustomerName,#txtAddress,#txtPhoneNo,#txtEmail").on('keydown',function (event){
-    if (event.key=Tab){
-        event.preventDefault();
-    }
-});
-
-
+// $("#txtCustomerId,#txtCustomerName,#txtAddress,#txtPhoneNo,#txtEmail").on('keydown',function (event){
+//     if (event.key=Tab){
+//         event.preventDefault();
+//     }
+// });
 /*---------------------------Item-------------------------------*/
 
 $('#btnSaveItem').click(function () {
@@ -134,17 +151,28 @@ $('#btnSaveItem').click(function () {
 function saveItem() {
     $('#tblItem>tr').off('click');
 
-    let itemBrand = $('#cmbBrandName').val();
+    let itemBrand = $("#cmbBrandName option:selected").text();
     let itemCode = $("#txtCode").val();
     let itemName = $("#txtItemName").val();
     let itemDescription = $("#txtDescription").val();
-    let itemNumber = $("#txtNumber").val();
+    let itemQty = $("#txtQty").val();
     let itemUnit = $("#txtUnitPrice").val();
     let itemGrand = $("#txtGrandTotal").val();
 
-    let row = `<tr><td>${itemBrand}</td><td>${itemCode}</td><td>${itemName}</td><td>${itemDescription}</td><td>${itemNumber}</td><td>${itemUnit}</td><td>${itemGrand}</td></tr>`;
+    let row = `<tr><td>${itemBrand}</td><td>${itemCode}</td><td>${itemName}</td><td>${itemDescription}</td><td>${itemQty}</td><td>${itemUnit}</td><td>${itemGrand}</td></tr>`;
 
     $('#tblItem').append(row);
+
+    let item = {
+        brand:itemBrand,
+        code:itemCode,
+        name:itemName,
+        description:itemDescription,
+        qty:itemQty,
+        unitPrice:itemUnit,
+        total:itemGrand
+    }
+    items.push(item);
 
     clearItemTextFields();
 
@@ -153,10 +181,12 @@ function saveItem() {
         let code = $(this).children('td:eq(1)').text();
         let iName = $(this).children('td:eq(2)').text();
         let description = $(this).children('td:eq(3)').text();
-        let number = $(this).children('td:eq(4)').text();
+        let qty = $(this).children('td:eq(4)').text();
         let unit = $(this).children('td:eq(5)').text();
         let grand = $(this).children('td:eq(6)').text();
     });
+
+
 }
 
 function clearItemTextFields() {
@@ -165,7 +195,7 @@ function clearItemTextFields() {
     $("#txtCode").val("");
     $("#txtItemName").val("");
     $("#txtDescription").val("");
-    $("#txtNumber").val("");
+    $("#txtQty").val("");
     $("#txtUnitPrice").val("");
     $("#txtGrandTotal").val("");
 
@@ -184,10 +214,10 @@ $("#txtItemName").on('keydown',function (event) {
 });
 $("#txtDescription").on('keydown',function (event) {
     if (event.key=="Enter"){
-        $("#txtNumber").focus();
+        $("#txtQty").focus();
     }
 });
-$("#txtNumber").on('keydown',function (event) {
+$("#txtQty").on('keydown',function (event) {
     if (event.key=="Enter"){
         $("#txtUnitPrice").focus();
     }
@@ -204,10 +234,69 @@ $("#txtGrandTotal").on('keydown',function (event) {
     }
 });
 
-$("#cmbBrandName,#txtCode,#txtItemName,#txtDescription,#txtNumber,#txtUnitPrice,#txtGrandTotal").on('keydown',function (event){
-    if (event.key=Tab){
-        event.preventDefault();
-    }
-});
+//
+// $("#cmbBrandName,#txtCode,#txtItemName,#txtDescription,#txtNumber,#txtUnitPrice,#txtGrandTotal").on('keydown',function (event){
+//     if (event.key=Tab){
+//         event.preventDefault();
+//     }
+// });
 
 /*--------------------------Order-------------------------------------*/
+
+function setCurrentDate(){
+    let orderDate = $('#orderDate');
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
+    orderDate.val(today);
+}
+
+function setOrderId(){
+    if (orders.length == 0){
+        $('#orderId').val(1);
+    }else{
+        let orderNo = orders.length;
+        $('#orderId').val(orderNo);
+    }
+}
+
+function addItemToDropDown(){
+    for(var i = 0; i<items.length;i++){
+        $('<option/>').val(items[i].code).html(items[i].code).appendTo('#cmbItem');
+    }
+}
+
+$('#cmbItem').change(function (){
+    let itemCode = $("#cmbItem option:selected").text();
+    let itemName;
+    for(let i = 0; i<items.length;i++){
+        if (itemCode === items[i].code){
+            itemName = items[i].name;
+        }
+    }
+    $('#orderItemName').val(itemName);
+});
+
+
+function addCustomerToDropDown(){
+    console.log("Customer drop down");
+    for(let i = 0; i<customers.length;i++){
+        console.log("Customer ID : "+customers[i].cusId);
+        $('<option/>').val(customers[i].cusId).html(customers[i].cusId).appendTo('#cmbCustomer');
+    }
+}
+
+
+$('#cmbCustomer').change(function (){
+    console.log("combo customer state change");
+    let cusId = $("#cmbCustomer option:selected").text();
+    console.log("State change Id : "+cusId);
+    for(let i = 0; i<customers.length;i++){
+        if (cusId === customers[i].cusId){
+            $('#orderCustomerName').val(customers[i].cusName);
+        }
+    }
+});
